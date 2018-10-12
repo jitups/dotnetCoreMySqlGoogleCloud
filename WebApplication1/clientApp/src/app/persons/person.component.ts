53080577e351f517763abc28d217a2c6250ef09c
Jitendra Surve <jitusurve@hotmail.com>
2018-10-12 10:01:29 +0530

    able to pass authorization header using angular code

--- WebApplication1/clientApp/src/app/persons/person.component.ts
+++ WebApplication1/clientApp/src/app/persons/person.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+//import { RequestOptions } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -9,12 +10,16 @@ export class PersonComponent {
   public persons: PersonViewModel[];
 
   constructor(http: HttpClient
-  //  , @Inject('BASE_URL') baseUrl: string
   ) {
     // console.log('https://mvcwebapi-215708.appspot.com/' + 'api/SampleData/Persons');
-    let headers = new HttpHeaders();
-    headers.append('Authorization', this.make_base_auth("webappuser", "Jitu@123"));
-    http.get<PersonViewModel[]>('https://mvcwebapi-215708.appspot.com/api/Person', { headers: headers }).subscribe(result => {
+    let baseUrl = 'https://mvcwebapi-215708.appspot.com/api/Person/';
+    let encryptedHeader = this.make_base_auth("webappuser", "Jitu@123");
+    let headers = new HttpHeaders({ 'Authorization' : encryptedHeader });
+    console.log('fetching data from ' + baseUrl);
+    //headers.set('Content-Type', 'application/json');
+    //headers.set('Authorization', this.make_base_auth("webappuser", "Jitu@123"));
+    //let options = new RequestOptions({ headers: headers });
+    http.get<PersonViewModel[]>(baseUrl , { headers: headers }).subscribe(result => {
       this.persons = result;
     }, error => console.error(error));
 
